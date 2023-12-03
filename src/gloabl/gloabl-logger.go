d17613f1a1fb51690db50b4ef17ef64ad05b1f56
smdnk <1808611881@qq.com>
2023-12-03 10:20:14 +0800

    日志目录初始化

--- src/gloabl/gloabl-logger.go
+++ src/gloabl/gloabl-logger.go
@@ -4,14 +4,31 @@ import (
 	"context"
 	lo "gorm.io/gorm/logger"
 	"log"
+	"os"
 )
 
-func initLogger() (wgLog lo.Interface) {
-	writer := log.Logger{}
-	config := lo.Config{
-		LogLevel: lo.Info,
+func openLog() (file *os.File) {
+	dir, _ := os.Getwd()
+	log.Printf("当前工作目录在%v", dir)
+	f, err := os.Create(dir + "\\" + "log\\" + "wg.log")
+	if err != nil {
+		log.Fatal("系统日志目录创建失败")
 	}
-	wgLog = lo.New(&writer, config)
+
+	return f
+}
+
+func initLogger() (wgLog lo.Interface) {
+	file := openLog()
+
+	writer := log.New(file, "\r\n", log.LstdFlags)
+	config := lo.Config{
+		LogLevel:                  lo.Info,
+		SlowThreshold:             200,
+		IgnoreRecordNotFoundError: true,
+		Colorful:                  true,
+	}
+	wgLog = lo.New(writer, config)
 	wgLog.Info(context.Background(), "全局日志初始化完成----")
 
 	return wgLog
